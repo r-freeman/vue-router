a658a1caae044b0b600a92e22a2f7951744a1d9c
Ryan Freeman <N00173767@student.iadt.ie>
2020-01-21 15:35:09 +0000

    Added about and contact routes

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home";
+import About from "./views/About";
+import Contact from "./views/Contact";
 
 Vue.use(Router);
 
@@ -9,6 +11,16 @@ const routes = [
         path: '/home',
         name: 'home',
         component: Home
+    },
+    {
+        path: '/about',
+        name: 'about',
+        component: About
+    },
+    {
+        path: '/contact',
+        name: 'contact',
+        component: Contact
     }
 ];
 

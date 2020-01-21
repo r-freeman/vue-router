89a4679b9d4cac04c5d12388035d0495a9dad13b
Ryan Freeman <N00173767@student.iadt.ie>
2020-01-21 15:24:52 +0000

    Added views/ and home.vue

---
+++ src/views/home.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="home">
+        <img src="../assets/logo.png" alt="">
+        <br>
+        <p>This is the home page</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'home',
+        components: {}
+    }
+</script>
+
+<style>
+.home {
+    text-align: center;
+}
+</style>

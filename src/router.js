e6dfc53a66a89cdf25e8e5e6d3f6122ca5c3d71f
Ryan Freeman <N00173767@student.iadt.ie>
2020-01-21 15:19:41 +0000

    Added router.js

---
+++ src/router.js
@@ -0,0 +1,16 @@
+import Vue from "vue";
+import Router from "vue-router";
+
+Vue.use(Router);
+
+const routes = [
+    {
+
+    }
+];
+
+export default new Router({
+    history: true,
+    base: null,
+    routes
+})
